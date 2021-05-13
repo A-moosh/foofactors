691844d3c9d4fb1c989cc565e2fc37d49b495207
A-moosh <81583106+A-moosh@users.noreply.github.com>
2021-05-13 15:04:10 -0500

    Make Factors Less Aggravating
    Factors have driven people to extreme measures, like ordering custom conference ribbons and laptop stickers to express how HELLNO we feel about stringsAsFactors. And yet, sometimes you need them. Can they be made less maddening? Let's find out.

---
+++ R/Untitled.R
@@ -0,0 +1,9 @@
+#' Medicaid Expenditure 2019.
+#'
+#' A dataset containing medicaid expenditure data in 2019
+#'
+#' @format A dataframe with 51 rows and 4 variables:
+#'
+#'
+#' @source  \url{https://github.com/GeoDaCenter/opioid-policy-scan/blob/master/data_final/PS06_2019_S.csv}
+"PS06"
--- R/fbinbd.R
+++ R/fbinbd.R
@@ -1,3 +1,16 @@
+#' Bind two factors
+#'#' Bind two factors
+#'
+#' Create a new factor from two existing factors, where the new factor's levels
+#' are the union of the levels of the input factors.
+#'
+#' @param a factor a
+#' @param b factor b
+#'
+#' @return a new factor that binds a and b
+#' @export
+#' @examples
+#' fbind(iris$Species[c(1, 51, 101)], PlantGrowth$group[c(1, 11, 21)])
 fbind <- function(a, b) {
   factor(c(as.character(a), as.character(b)))
 }
